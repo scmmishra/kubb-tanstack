c380c32bd493565ca0cbd8258b5cc6096c9f192f
Shivam Mishra <scm.mymail@gmail.com>
2024-01-14 19:53:34 +0530

    feat: base query

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
+// import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
 </script>
 
 <template>
-  <VueQueryDevtools />
+  <Suspense>
+    <RouterView />
+  </Suspense>
 </template>
 
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { useFindPetsByStatus } from '@/api/hooks'
+import { computed } from 'vue'
+
+const pets = await useFindPetsByStatus(
+  { status: 'available' },
+  {
+    client: {
+      baseURL: 'https://petstore3.swagger.io/api/v3/'
+    }
+  }
+)
+
+const allPets = computed(() => pets.data.value)
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="text-gray-900">
+    <h1>All Pets</h1>
+    <pre>{{ allPets }}</pre>
   </main>
 </template>
